f1c174efca87d3d5ab8988227c51865035250fd6
scristiansc <scristian1012@gmail.com>
2025-06-18 21:53:53 -0500

    In progress

---
+++ config.js
@@ -0,0 +1,71 @@
+window.PorfolioConfig = {
+  name: 'Cristian Suárez',
+  jobTitle: 'Software Developer',
+  jobDescription: 'Im an idea builder, and full-time problem solver. I specialize in turning lines of code into smooth, scalable, and user-friendly experiences.From captivating interfaces to rock-solid backend systems. I mix creativity, logic, and just the right amount of coffee to build tech that truly matters.',
+  skills: [
+    {
+      name: 'JavaScript',
+      description: 'Reprehenderit mollit id ad veniam.'
+    },
+    {
+      name: 'HTML',
+      description: 'Fugiat veniam esse consequat quis cillum elit consequat Lorem pariatur ipsum veniam irure incididunt non.'
+    },
+    {
+      name: 'CSS',
+      description: 'Elit tempor mollit consectetur laborum nostrud qui in.'
+    }
+  ],
+  works: [
+    {
+      name: 'Vue Stripe Checkout',
+      link: 'https://jofftiquez.github.io/vue-stripe-checkout',
+      description: 'Mollit dolor tempor adipisicing non pariatur in tempor cillum do. In officia ullamco laborum ullamco voluptate. Laboris sunt quis enim Lorem laboris aliquip duis non magna excepteur voluptate. Pariatur velit aliquip officia nulla anim reprehenderit deserunt. Sint nostrud ipsum do aliquip.'
+    },
+    {
+      name: 'ToDo App',
+      link: 'https://theuselessweb.com/',
+      description: 'Esse magna anim incididunt enim sint aute consectetur ipsum id nostrud eu do. Pariatur et anim enim aliquip anim. Laboris labore incididunt labore enim nisi elit excepteur deserunt irure exercitation exercitation. Lorem sit ad laborum sit velit veniam sit nisi nostrud occaecat duis nulla duis.'
+    },
+    {
+      name: 'A Mobile Game',
+      link: 'https://theuselessweb.com/',
+      description: 'Incididunt non cupidatat deserunt ut commodo qui non do Lorem. Qui aute minim enim sit magna est consectetur incididunt. Ex est cupidatat tempor pariatur deserunt labore tempor minim laboris magna ipsum minim ea ea. Consectetur do anim laboris nulla nisi veniam sint qui.'
+    }
+  ],
+  hobbies: [
+    {
+      name: 'Rock Climbing',
+      description: 'Occaecat ipsum dolor ea ad ut irure do proident excepteur consectetur duis eu culpa velit.'
+    },
+    {
+      name: 'Surfing',
+      description: 'Laboris occaecat adipisicing culpa duis aute ea excepteur quis eiusmod mollit est pariatur.'
+    },
+    {
+      name: 'Rubick\'s Cube',
+      description: 'Laborum pariatur sint labore officia enim occaecat id.'
+    }
+  ],
+  contacts: {
+    email: 'scristian1012@gmail.com',
+    phoneNo: '+57 3013674088',
+    links: [
+      {
+        name: 'GitHub',
+        link: 'https://github.com/scristiansc',
+        icon: 'mdi-github'
+      },
+      {
+        name: 'Linkedin',
+        link: 'https://www.linkedin.com/in/cristian-su%C3%A1rez-c/',
+        icon: 'mdi-linkedin'
+      },
+      {
+        name: 'Gmail',
+        link: 'mailto:scristian1012@gmail.com',
+        icon: 'mdi-gmail'
+      }
+    ]
+  }
+};
